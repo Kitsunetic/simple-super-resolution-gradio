7d0a37b19a7f5181ae6dcc9d84e43cd6861c2ad2
Kitsunetic <jh.shim.gg@gmail.com>
2025-11-16 04:07:57 +0000

    update version

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import find_packages, setup
 
 setup(
     name="gradio-sr-app",
-    version="0.0.0",
+    version="0.0.1",
     packages=find_packages(),
     py_modules=["sr"],
     install_requires=[
